bc5c8cb2d8d1718c683e0519ce05ca506abd538d
isaccanedo <isaccanedo@gmail.com>
2021-07-30 15:39:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+![Image of Coinmarketcap Reactjs](https://i.imgur.com/isl3wwa.png)
+
+Installation.
+-------------
+
+```
+git clone https://github.com/mariodev12/coinmarketcap.git 
+cd coinmarketcap 
+npm install 
+cd /server 
+npm install
+```
+
+Once everything is installed start the server made with NodeJS inside the server folder.
+
+node index.js
+
+After that start the development server for reactjs inside the folder, coinmarketcap.
+
+npm start.
+
+API.
+----------
+
+- [Coinmarketcap](https://coinmarketcap.com/api/).
+
+Folder Structure
+----------------
+
+Your project should look like this:
+
+```
+coinmarketcap/
+  README.md
+  node_modules/
+  package.json
+  public/
+    index.html
+    favicon.ico
+    manifest.json
+  server/
+    node_modules/
+    index.js
+    package-lock.json
+    package.json
+  src/
+    content/
+      Home/
+        content.css
+        content.js
+      Page/
+        content.css
+        content.js
+    details/
+      details.css
+      details.js
+    footer/
+    header/
+      header.css
+      header.js
+    pagination/
+      pagination.css
+      pagionationCustom.js
+    App.css
+    App.js
+    App.test.js
+    index.css
+    index.js
+    logo.svg
+    registerServiceWorker.js
+```
+
+Roadmap
+-------
+
+- [ ] Real time data
+- [ ] Redux
+- [ ] Portfolio
+- [ ] fix a bunch of bugs
+
+Thanks
+------
+
+**Mario Diez** © 2017+, Mario Diez. Released under the [MIT License].<br>
+Authored and maintained by Mario Diez.
+
+> [mariodiezgarcia.com](http://www.mariodiezgarcia.com) &nbsp;&middot;&nbsp;
+> GitHub [@mariodev12](https://github.com/mariodev12) &nbsp;&middot;&nbsp;
+> Youtube [@mariodiezoficial](https://www.youtube.com/channel/UCisGMoxaVxJMcbio2FBHORg) &nbsp;&middot;&nbsp;
+> Twitter [@mariodev_](https://twitter.com/mariodev_)
+
+[MIT License]: http://mit-license.org/
---
+++ src/content/Home/content.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import {Table} from 'reactstrap';
+import axios from 'axios';
+import {Link} from  'react-router-dom'
+import numeral from 'numeral'
+
+require('./content.css');
+
+class Content extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: null,
+            id: null,
+        }
+    }
+    componentWillMount() {
+        var that = this;
+        axios.all([
+            axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=100'),
+            axios.get('https://s2.coinmarketcap.com/generated/search/quick_search.json')
+        ])
+        .then(axios.spread(function (data, list) {
+            var result = list.data.reduce(function(obj,item){
+                obj[item.name] = item.id; 
+                return obj;
+                }, {});
+            that.setState({
+                data: data.data,
+                id: result
+            })
+        }))
+        .catch(error => console.log(error));
+
+    }
+
+    convertSpace = (string) => {
+        return string.split(' ').join('-')
+    }
+    /*
+    renderRow(){
+        return this.state.data.map((item, k) => {
+            return (
+                    <tr key={k}>
+                        <th scope="row">{item.rank}</th>
+                        <td>
+                            <img alt={this.state.id[item.name]} src={`https://s2.coinmarketcap.com/static/img/coins/32x32/${this.state.id[item.name]}.png`} />
+                            <Link key={k} to={`/${this.state.id[item.name]}/${this.convertSpace(item.name)}`}>{item.name}</Link>
+                        </td>
+                        <td className="rightText">{numeral(item.market_cap_usd).format('$0,0[.]00')}</td>
+                        <td className="rightText">{numeral(item.price_usd).format('$0,0[.]00')}</td>
+                        <td className="rightText">{numeral(item['24h_volume_usd']).format('$0,0[.]00')}</td>
+                        <td className="rightText">{numeral(item.available_supply).format('0,0.0000')} {item.symbol}</td>
+                        <td className="rightText">{item.percent_change_24h}</td>
+                    </tr>
+            )
+        })
+    }
+    */
+    render(){
+        return (
+            <div className="container-home">
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th className="rightText">Market Cap</th>
+                            <th className="rightText">Price</th>
+                            <th className="rightText">Volume (24h)</th>
+                            <th className="rightText">Circulating Supply</th>
+                            <th className="rightText">Change (24h)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.data ? this.renderRow() : null}
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
+}
+
+export default Content;
